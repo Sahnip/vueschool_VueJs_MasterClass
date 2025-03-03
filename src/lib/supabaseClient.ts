f0ad0930dd57b7fe114ab94554415d7d26dbaf7b
S4hn1p <pinhas.roumani@gmail.com>
2025-03-03 02:16:54 +0100

    Config supabase + Sql request

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient('https://rorvijjbusaizgahbzkb.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvcnZpampidXNhaXpnYWhiemtiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA5MzI2MjcsImV4cCI6MjA1NjUwODYyN30.ls8LRHHT6qu3AjrAMyYqa4Qryb7otJ4y-yh81DF72i8')
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY)
 
-
+console.log(import.meta.env.VITE_SUPABASE_URL)
 export default supabase;
